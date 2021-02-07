024ba92872db2c0331f570258b43a59cdfe6776f
Ashley Kobayashi <ashleykobayashi@Ashleys-MacBook-Pro.local>
2021-02-06 19:13:16 -0800

    pair session: re-design addDataset by first unzipping with loadAsync, and then trying to append all of the dataset's contents into a resulting string. .folder and .file were added but still need to confirm what is happening in combination with .async

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -21,7 +21,7 @@ export default class InsightFacade implements IInsightFacade {
 
     public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
         // check id validity
-        if (!id.endsWith(".zip") || id.includes("_") || !id.trim() || id === "") {
+        if (id.includes("_") || !id.trim() || id === "") {
             return Promise.reject(new InsightError("invalid id"));
         } else {
             // check kinds validity
@@ -32,19 +32,46 @@ export default class InsightFacade implements IInsightFacade {
                 if (fs.existsSync(".data/" + id + ".zip")) {
                     return Promise.reject(new InsightError("file already added"));
                 } else {
-                    // confirm with TA
-                    fs.writeFileSync(".data/" + id + ".zip", content);
                     // unzip
                     let zip = new JSZip();
-                    zip.loadAsync(content).then(function (r) {
+                    zip.loadAsync(content, { base64: true }).then(function (r) {
+                        // console.log(r);
                         // doParse(r);
+                        // r is JSzip, r.files -> courses/, and courses/DEPTNUM
+                        for (let item in r.files) {
+                            // before adding, pick out info we want (sfield and mfield)
+                            // for every DEPTNUM access content, convert to string, put in data director
+                            // datasets[id] -> string that holds the content for that dataset
+                            // datasets[ZOOLonly] -> string: "course: ... id: ..., avg: ..., pass: ... "
+                            // datasets[id] -> dir -> course -> content
+                            let result: string;
+                            if (item.endsWith("/")) { // or dir == true
+                                r.folder(item);
+                            } else {
+                                r.file(item)
+                                    .async("string")
+                                    .then(function success() {
+                                        // maybe success(content)
+                                        // use the content
+                                        result.concat(content);
+                                    }, function error(e) {
+                                        // handle the error
+                                    });
+                            }
+                        }
+                    }, function error(e) {
+                        // handle error
                     });
-                    // @ts-ignore
-                    return Promise.reject("Not implemented.");
                 }
             }
         }
+        // confirm with TA
+        // fs.writeFileSync(".data/" + id + ".zip", content);
+        // @ts-ignore
+        return Promise.reject("Not implemented.");
     }
+
+
     public removeDataset(id: string): Promise<string> {
         return Promise.reject("Not implemented.");
     }
