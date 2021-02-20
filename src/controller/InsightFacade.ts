1bea36825a4d572a44e8aedf93f5a2a0144f18b7
Ashley Kobayashi <ashleykobayashi@Ashleys-MacBook-Pro.local>
2021-02-19 18:06:32 -0800

    comments on overall performQuery algo, abstracted validation methods for validateQuery, validateWHERE, validateOPTIONS, validateORDER, splitIDKey (for splitting courses_avg for e.g.), isValidField to call after splitting. 64 local tests now passing. Temporarily moved big block of comments into /queryModel/notesfromperformquqery.ts to hit 300 lines in InsightFacade.ts file but need to reorganize

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,5 +1,6 @@
 import Log from "../Util";
-import {IInsightFacade, InsightDataset, InsightDatasetKind, InsightError, NotFoundError} from "./IInsightFacade";
+import {IInsightFacade, InsightDataset, InsightDatasetKind, InsightError, NotFoundError, ResultTooLargeError
+} from "./IInsightFacade";
 import {Dataset} from "../dataModel/Dataset";
 import * as JSZip from "jszip";
 import * as fs from "fs";
@@ -14,13 +15,15 @@ import {Section} from "../dataModel/Section";
 export default class InsightFacade implements IInsightFacade {
     public currentDatasets: string[] = [];
     public datasetsMap: Map<string, Dataset>;
+    public allFields: string[] = ["avg", "pass", "fail", "audit", "year", "dept", "id", "instructor", "title", "uuid"];
+    public sFields: string[] = ["dept", "id", "instructor", "title", "uuid"];
+    public mFields: string[] = ["avg", "pass", "fail", "audit", "year"];
     constructor() {
         Log.trace("InsightFacadeImpl::init()");
         this.datasetsMap = new Map();
     }
 
 // TODO: valid zip file check
-    // extractJson method
     public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
         // check id validity
         if (id.includes("_") || !id.trim() || id === "") {
@@ -114,7 +117,6 @@ export default class InsightFacade implements IInsightFacade {
         return newDataset;
     }
 
-
     public setSectionFields(newSection: Section, section: any) {
         if (section.Section === "overall") {
             newSection.setYear(1900);
@@ -131,7 +133,6 @@ export default class InsightFacade implements IInsightFacade {
         newSection.setInstructor(section.Professor);
         newSection.setTitle(section.Title);
     }
-
 
     // return promise of updated currentDatasets
     public removeDataset(id: string): Promise<string> {
@@ -177,7 +178,6 @@ export default class InsightFacade implements IInsightFacade {
         return Promise.resolve(result);
     }
 
-
     // check if query is valid - options first, then where
     // Abstract data tree for query (nested object)
     // grab the data
@@ -185,9 +185,115 @@ export default class InsightFacade implements IInsightFacade {
     // check the query, do the filtering with the dataset
     // read the query into our query data struct
     // for every level in the query, we want to
+    // input: query: JSON -> JSON
+    // instance of insightfacade for our dataset. pass this in
     public performQuery(query: any): Promise<any[]> {
-        return Promise.reject("Not implemented.");
+        // check if query is valid
+        if (!this.validateQuery(query)) {
+            return Promise.reject(new InsightError("Query is invalid")); }
     }
 
+    // first check if dataset is in our map, then check if dataset being queried has been added to data dir (disk)
+    // json.parse
+    public validateQuery(query: any): Promise<any> {
+        // check if query is empty
+        if (Object.keys(query).length === 0) {
+            return Promise.reject(new InsightError("Query is empty")); }
+        this.validateWHERE(query);
+        this.validateOPTIONS(query);
+
+        // check if WHERE keys are all valid
+    }
+
+    public validateWHERE(query: any): Promise<any> {
+        // check if WHERE clause exists
+        if (Object.keys(query).includes("WHERE")) {
+            return Promise.reject(new InsightError("Query must contain valid WHERE")); }
+        // check if WHERE clause exists but is empty
+        if (!query["WHERE"]) {
+            return Promise.reject(new ResultTooLargeError("Matches all entries.")); }
+    }
+
+    public validateOPTIONS(query: any): Promise<any> {
+        if (!Object.keys(query).includes("OPTIONS")) {
+            return Promise.reject(new InsightError("Query must contain valid OPTIONS")); }
+        // check if OPTIONS clause exists but is empty
+        if (query.OPTIONS.length === 0) {
+            return Promise.reject(new InsightError("Query must contain non-empty OPTIONS")); }
+
+        // check validity of COLUMNS
+        if (Object.keys(query.OPTIONS).length === 1 || Object.keys(query.OPTIONS).length === 2) {
+            this.validateCOLUMNS(query);
+        } else {
+            return Promise.reject(new InsightError("OPTIONS must have one or two clauses")); }
+
+        // check validity of ORDER if it exists
+        if (Object.keys(query.OPTIONS).length === 2) {this.validateORDER(query); }
+        // todo: check that the key is in column also
+        // todo: CONFIRM that the order of keys must be columns, then order? or either?
+        // ORDER has one key, check that it is inside COLUMNS
+        // if ORDER exists, check that it only contains one key
+
+        // IF BLOCK: COLUMNS and ORDER case
+        if (Object.keys(query.OPTIONS).length === 2) {
+            if (Object.keys(query.OPTIONS)[0] !== "COLUMNS" || Object.keys(query.OPTIONS)[1] !== "ORDER") {
+                return Promise.reject(new InsightError("OPTIONS must contain COLUMNS then ORDER")); }
+
+            // check if COLUMNS is empty
+            if (Object.keys(query.OPTIONS.COLUMNS).length === 0) {
+                return Promise.reject(new InsightError("COLUMNS cannot be empty")); } }
+    }
+
+    public validateORDER(query: any): Promise<any> {
+            // if OPTIONS contains a second OPTION, check that it is ORDER
+            // Check if OPTIONS contains ORDER (optional clause)
+
+        if (Object.keys(query.OPTIONS).length !== 2) {
+            return Promise.reject(new InsightError("COLUMNS can only contain one clause")); }
+        // when OPTIONS contains ORDER, check that OPTIONS only has one ORDER
+        if (Object.keys(query.OPTIONS)[1] !== "ORDER") {
+            return Promise.reject(new InsightError("OPTIONS must have one or no ORDER")); }
+
+        // check if ORDER has one key only
+        // todo: confirm whether this is okay way to access ORDER (is it itself a sub object?)
+        if (Object.keys(query.OPTIONS.ORDER).length !== 1) {
+            return Promise.reject(new InsightError("ORDER can only contain one key")); }
+        // check if ORDER's (singular) key is valid skey or mkey
+        if (!this.isValidField(Object.keys(query.OPTIONS.ORDER)[0], "all")) {
+            return Promise.reject(new InsightError("Invalid key")); }
+    }
+
+    public validateCOLUMNS(query: any): Promise<any> {
+        // check that COLUMNS has at least one key
+        if (Object.keys(query.OPTIONS.COLUMNS).length < 1) {
+            return Promise.reject(new InsightError("COLUMNS must contain at least one key")); }
+
+        // check that OPTIONS contains COLUMNS
+        if (Object.keys(query.OPTIONS).length === 1) {
+            if (Object.keys(query.OPTIONS)[0] !== "COLUMNS") {
+                return Promise.reject(new InsightError("OPTIONS must contain COLUMNS")); } }
+        // probably duplicated logic from the above
+        if (Object.keys(query.OPTIONS)[0] !== "COLUMNS") {
+            return Promise.reject(new InsightError("OPTIONS must contain COLUMNS")); }
+
+        // check the validity of each key inside COLUMNS
+        // for each id_key in columns, split, and check that the key element is one of the valid mkeys or skeys
+        for (const key of Object.keys(query.OPTIONS.COLUMNS)) {
+            let currIDKeyArr = this.splitIDKey(key);
+            let currKey: string = currIDKeyArr[1];
+            if (!this.isValidField(currKey, "all")) {
+                return Promise.reject(new InsightError("Invalid key")); } }
+    }
+
+    public splitIDKey(idKey: string): string[] {return idKey.split("_"); }
+
+    public isValidField(field: string, fieldType: string): boolean {
+        if (fieldType === "all") {
+            if (this.allFields.includes(field)) {return true; }
+        } else if (fieldType === "sField") {
+            if (this.sFields.includes(field)) {return true; }
+        } else if (fieldType === "mField") {
+            if (this.mFields.includes(field)) {return true; }
+        } else {return false; } }
 
 }
---
+++ src/queryModel/notesfromperformquery.ts
@@ -0,0 +1,20 @@
+// todo: call validate filter on each (type checking key:val pairs)
+// todo: validate that all dataset ids match and are valid
+// public handleFilter() {
+//     switch:
+//         case: "AND" {
+//             // for each Object.keys inside this AND sub-object, recurse handleFilter(sub-subobject)
+//         }
+//         case: "OR"{
+//             // for each Object.keys inside this AND sub-object, recurse handleFilter(sub-subobject)
+//         }
+//         case: "NOT"
+//     // single case ones are based objects
+//         // do the filtering with the dataset
+//         case: "LT"
+//     // do the filtering with the dataset
+//         case: "GT"
+//         case: "EQ"
+//         case: "IS"
+//         default:
+// }
