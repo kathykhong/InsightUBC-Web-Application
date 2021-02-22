61281f49934835a4c0ff9b1fa11f63c1859b3601
Ashley Kobayashi <ashleykobayashi@Ashleys-MacBook-Pro.local>
2021-02-21 16:12:53 -0800

    attempt at validateLT but missing handling of recursive layers, extracted isValidIDString(string), added some checks into validateORDER() and validateCOLUMNS(), reordered QueryValidator so that validateCOLUMNS comes before validateORDER method

--- src/queryModel/QueryValidator.ts
+++ src/queryModel/QueryValidator.ts
@@ -10,6 +10,8 @@ export class QueryValidator {
     public logicFilters: string[] = ["AND", "OR"];
     public mCompareFilters: string[] = ["LT", "GT", "EQ"];
     public sCompareFilters: string[] = ["IS"];
+    public columnIDString: string;
+    public columnFields: string[] = [];
     public insightFacade: InsightFacade = new InsightFacade();
 
     // first check if dataset is in our map, then check if dataset being queried has been added to data dir (disk)
@@ -28,7 +30,6 @@ export class QueryValidator {
     // todo: validate that all dataset ids match and are valid
 
 
-    // todo: figure out how the recursive calls work, what are we passing in to get all filters for the next level
     public validateWHERE(query: any): Promise<any> {
         // check if WHERE clause exists
         if (Object.keys(query).includes("WHERE")) {
@@ -51,13 +52,13 @@ export class QueryValidator {
 
     // if its an M or Scomp, return
     // if its an Logic or Negation, recurse???
-
+    // todo: figure out how the recursive calls work, what are we passing in to get all filters for the next level
     // todo: Object.keys(query.WHERE)[0] can't be called at for each level of recursion???? help
     // todo: do we need resolve and a break or ??
     public validateFilter(query: any): Promise<any> {
         switch (Object.keys(query.WHERE)[0]) {
             case "LT": {
-                // validateLT(); to check the value?
+                this.validateLT(query);
                 return Promise.resolve();
                 break;
             }
@@ -94,6 +95,41 @@ export class QueryValidator {
         }
     }
 
+    // todo: what are we passing in though
+    public validateLT(query: any): Promise<any> {
+        // check that LT is not empty
+        if (Object.keys(query.WHERE.LT).length !== 1) {
+            return Promise.reject(new InsightError("LT block must have one idstring_field argument"));
+        }
+        // check that LT's key:value value is a number
+        let argLT = Object.keys(query.WHERE.LT)[0];
+        if (typeof query.WHERE.LT.argLT !== "number") {
+            return Promise.reject(new InsightError("LT must compare to a number"));
+        }
+        // check that LT is calling on a valid mfield
+        let idStringLT: string[];
+        if (argLT.includes("_")) {
+            idStringLT = this.splitIDKey(argLT);
+        } else {
+            return Promise.reject(new InsightError("invalid key format. Must separate idstring from field"));
+        }
+
+        if (!this.mFields.includes(idStringLT[1])) {
+            return Promise.reject(new InsightError("invalid mfield specified"));
+        }
+
+        // check that the id is valid
+        if (!this.isValidIDString(idStringLT[0])) {
+            return Promise.reject(new InsightError("invalid ID"));
+        }
+
+        // check that id matches all other ids in the query
+        if (idStringLT[0] !== this.columnIDString) {
+            return Promise.reject(new InsightError("dataset ID must match the rest of the query"));
+        }
+
+    }
+
     public validateOPTIONS(query: any): Promise<any> {
         if (!Object.keys(query).includes("OPTIONS")) {
             return Promise.reject(new InsightError("Query must contain valid OPTIONS"));
@@ -112,7 +148,6 @@ export class QueryValidator {
 
         // check validity of ORDER if it exists
         if (Object.keys(query.OPTIONS).length === 2) {this.validateORDER(query); }
-        // todo: check that the key is in column also
         // todo: CONFIRM that the order of keys must be columns, then order? or either?
         // ORDER has one key, check that it is inside COLUMNS
         // if ORDER exists, check that it only contains one key
@@ -130,28 +165,6 @@ export class QueryValidator {
         }
     }
 
-    public validateORDER(query: any): Promise<any> {
-        // if OPTIONS contains a second OPTION, check that it is ORDER
-        // Check if OPTIONS contains ORDER (optional clause)
-
-        if (Object.keys(query.OPTIONS).length !== 2) {
-            return Promise.reject(new InsightError("COLUMNS can only contain one clause"));
-        }
-        // when OPTIONS contains ORDER, check that OPTIONS only has one ORDER
-        if (Object.keys(query.OPTIONS)[1] !== "ORDER") {
-            return Promise.reject(new InsightError("OPTIONS must have one or no ORDER"));
-        }
-
-        // check if ORDER has one key only
-        // todo: confirm whether this is okay way to access ORDER (is it itself a sub object?)
-        if (Object.keys(query.OPTIONS.ORDER).length !== 1) {
-            return Promise.reject(new InsightError("ORDER can only contain one key"));
-        }
-        // check if ORDER's (singular) key is valid skey or mkey
-        if (!this.isValidField(Object.keys(query.OPTIONS.ORDER)[0], "all")) {
-            return Promise.reject(new InsightError("Invalid key"));
-        }
-    }
 
     public validateCOLUMNS(query: any): Promise<any> {
         // check that COLUMNS has at least one key
@@ -165,23 +178,76 @@ export class QueryValidator {
                 return Promise.reject(new InsightError("OPTIONS must contain COLUMNS"));
             }
         }
-        // probably duplicated logic from the above
-        if (Object.keys(query.OPTIONS)[0] !== "COLUMNS") {
-            return Promise.reject(new InsightError("OPTIONS must contain COLUMNS"));
-        }
 
         // check the validity of each key inside COLUMNS
         // for each id_key in columns, split, and check that the key element is one of the valid mkeys or skeys
+        let prevID: string = "";
         for (const key of Object.keys(query.OPTIONS.COLUMNS)) {
             let currIDKeyArr = this.splitIDKey(key);
+            let currID: string = currIDKeyArr[0];
             let currKey: string = currIDKeyArr[1];
             if (!this.isValidField(currKey, "all")) {
                 return Promise.reject(new InsightError("Invalid key"));
             }
+            this.columnFields.push(currKey);
+
+            if ((currID.includes("_") || !currID.trim())) {
+                return Promise.reject(new InsightError("Invalid course id"));
+            }
+            // check if all keys have the same id
+            if (prevID === "" || prevID === currID) {
+                prevID = currID;
+            } else {
+                    return Promise.reject(new InsightError("all keys must have the same id"));
+                }
+            }
+        this.columnIDString = prevID;
+        }
+
+    public validateORDER(query: any): Promise<any> {
+        // if OPTIONS contains a second OPTION, check that it is ORDER
+        // Check if OPTIONS contains ORDER (optional clause)
+        // maybe take the following out (in case we can't guarantee the checking at the caller level)
+        if (Object.keys(query.OPTIONS).length !== 2) {
+            return Promise.reject(new InsightError("COLUMNS can only contain one clause"));
+        }
+        // when OPTIONS contains ORDER, check that OPTIONS only has one ORDER
+        if (Object.keys(query.OPTIONS)[1] !== "ORDER") {
+            return Promise.reject(new InsightError("OPTIONS must have one or no ORDER"));
+        }
+
+        // check if ORDER has one key only
+        if (Object.keys(query.OPTIONS.ORDER).length !== 1) {
+            return Promise.reject(new InsightError("ORDER can only contain one key"));
+        }
+        // check if ORDER's (singular) key is valid skey or mkey
+        let currIDKeyArr = this.splitIDKey(Object.keys(query.OPTIONS.ORDER)[0]);
+        let currID: string = currIDKeyArr[0];
+        let currKey: string = currIDKeyArr[1];
+        if (!this.isValidField(currKey, "all")) {
+            return Promise.reject(new InsightError("Invalid key"));
+        }
+        if (this.columnIDString !== currID) {
+            return Promise.reject(new InsightError("Dataset ID must match all other dataset IDs in this query"));
+        }
+
+        // check if ORDER's (singular) key is included in COLUMNS
+        if (!this.columnFields.includes(currKey)) {
+            return Promise.reject(new InsightError("ORDER key must be included in COLUMNS keys "));
         }
     }
 
     public splitIDKey(idKey: string): string[] {return idKey.split("_"); }
+
+    public isValidIDString(ID: string): boolean {
+        if (ID.length < 1) {
+            return false;
+        }
+        if (ID.includes("_")) {
+            return false;
+        }
+        return true;
+    }
 
     public isValidField(field: string, fieldType: string): boolean {
         if (fieldType === "all") {
