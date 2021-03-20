92a4d9b248a7edcb1db0fc9eb3a82741ece739d5
Ashley Kobayashi <ashleykobayashi@Ashleys-MacBook-Pro.local>
2021-03-19 17:44:26 -0700

    C2 QUERY STUF

--- src/dataModel/Room.ts
+++ src/dataModel/Room.ts
@@ -16,44 +16,92 @@ export class Room {
         this.latLon = data;
     }
 
+    public getLatLon(): any {
+        return this.latLon;
+    }
+
+    public getLat(): number {
+        return this.latLon.lat;
+    }
+
+    public getLon(): number {
+        return this.latLon.lon;
+    }
+
     public setFullname(fullname: string) {
         this.fullname = fullname;
+    }
+
+    public getFullname(): string {
+        return this.fullname;
     }
 
     public setShortname(shortname: string) {
         this.shortname = shortname;
     }
 
+    public getShortname(): string {
+        return this.shortname;
+    }
+
     public setNumber(roomNumber: string) {
         this.number = roomNumber;
+    }
+
+    public getNumber(): string {
+        return this.number;
     }
 
     public setName(name: string) {
         this.name = name;
     }
 
+    public getName(): string {
+        return this.name;
+    }
+
     public setAddress(address: string) {
         this.address = address;
+    }
+
+    public getAddress(): string {
+        return this.address;
     }
 
     public setSeats(seats: number) {
         this.seats = seats;
     }
 
+    public getSeats(): number {
+        return this.seats;
+    }
+
     public setType(type: string) {
         this.type = type;
+    }
+
+    public getType(): string {
+        return this.type;
     }
 
     public setFurniture(furniture: string) {
         this.furniture = furniture;
     }
 
+    public getFurniture(): string {
+        return this.furniture;
+    }
+
     public setLink(link: string) {
         this.href = link;
     }
 
-    public getRoomShortname() {
-        return this.shortname;
+    public getLink(): string {
+        return this.href;
+    }
+
+    public getRoomArg(anykey: string): string {
+      return "";
     }
 
 }
